0a05bb53f7adaa7c67fadcded04de737e38c2c93
aravind <g.aravindadithya@gmail.com>
2025-07-03 19:53:02 +0530

    cifar_M_weight

---
+++ experiments/rfm_mnist/M_fc_rfm_1 (1).h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b2f8c556cde5c8e661d74c0b8806f345a65d327543c2662e2ae6f7744c4df9c0
+size 37755298
---
+++ experiments/rfm_mnist/M_fc_rfm_2 (1).h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a9717f6f1c80d608a2438314e0831c882a4c53615df168a8a4549b8dc92fac97
+size 37755298
